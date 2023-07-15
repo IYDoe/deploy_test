5ab3ef74879254bcd97ef19a5bdbec4a64c3355b
Ilya <ilya.yudin@metamins.com>
2023-07-15 14:55:00 +0500

    porfile

--- packages/client/src/pages/ProfilePage.tsx
+++ packages/client/src/pages/ProfilePage.tsx
@@ -24,66 +24,68 @@ export const ProfilePage: FC = () => {
         axios.post('http://localhost:3000/api/topics')
     }, [])
 
-    return (
-        <main className="main">
-            <div className="shape">
-                <ButtonBack />
-                <div className="shape__wrapper">
-                    <div className="profile">
-                        <div className="profile__inner">
-                            <Avatar />
+    return <div>test</div>
 
-                            <Link to={Paths.editProfile} className="link">
-                                Изменить данные
-                            </Link>
-                            <Link to={Paths.editPassword} className="link">
-                                Изменить пароль
-                            </Link>
-                            <div
-                                className="link link_warning"
-                                onClick={handleLogout}>
-                                Выйти
-                            </div>
-                        </div>
-                        <ul className="profile__list">
-                            <li className="profile__item">
-                                <span className="profile__lettering">Имя</span>
-                                {first_name}
-                            </li>
-                            <li className="profile__item">
-                                <span className="profile__lettering">
-                                    Фамилия
-                                </span>
-                                {second_name}
-                            </li>
-                            <li className="profile__item">
-                                <span className="profile__lettering">
-                                    Имя в чате
-                                </span>
-                                {display_name}
-                            </li>
-                            <li className="profile__item">
-                                <span className="profile__lettering">
-                                    Логин
-                                </span>
-                                {login}
-                            </li>
-                            <li className="profile__item">
-                                <span className="profile__lettering">
-                                    Почта
-                                </span>
-                                {email}
-                            </li>
-                            <li className="profile__item">
-                                <span className="profile__lettering">
-                                    Телефон
-                                </span>
-                                {phone}
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </main>
-    )
+    // return (
+    // <main className="main">
+    //     <div className="shape">
+    //         <ButtonBack />
+    //         <div className="shape__wrapper">
+    //             <div className="profile">
+    //                 <div className="profile__inner">
+    //                     <Avatar />
+
+    //                     <Link to={Paths.editProfile} className="link">
+    //                         Изменить данные
+    //                     </Link>
+    //                     <Link to={Paths.editPassword} className="link">
+    //                         Изменить пароль
+    //                     </Link>
+    //                     <div
+    //                         className="link link_warning"
+    //                         onClick={handleLogout}>
+    //                         Выйти
+    //                     </div>
+    //                 </div>
+    //                 <ul className="profile__list">
+    //                     <li className="profile__item">
+    //                         <span className="profile__lettering">Имя</span>
+    //                         {first_name}
+    //                     </li>
+    //                     <li className="profile__item">
+    //                         <span className="profile__lettering">
+    //                             Фамилия
+    //                         </span>
+    //                         {second_name}
+    //                     </li>
+    //                     <li className="profile__item">
+    //                         <span className="profile__lettering">
+    //                             Имя в чате
+    //                         </span>
+    //                         {display_name}
+    //                     </li>
+    //                     <li className="profile__item">
+    //                         <span className="profile__lettering">
+    //                             Логин
+    //                         </span>
+    //                         {login}
+    //                     </li>
+    //                     <li className="profile__item">
+    //                         <span className="profile__lettering">
+    //                             Почта
+    //                         </span>
+    //                         {email}
+    //                     </li>
+    //                     <li className="profile__item">
+    //                         <span className="profile__lettering">
+    //                             Телефон
+    //                         </span>
+    //                         {phone}
+    //                     </li>
+    //                 </ul>
+    //             </div>
+    //         </div>
+    //     </div>
+    // </main>
+    // )
 }
