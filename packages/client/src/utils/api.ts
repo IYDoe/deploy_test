f0f541026ee9c635bcdd73f238c4afd50b97163b
Ilya <ilya.yudin@metamins.com>
2023-07-15 18:11:03 +0500

    test

--- packages/client/src/utils/api.ts
+++ packages/client/src/utils/api.ts
@@ -1,6 +1,6 @@
 const prodUrl = 'https://game-machine.ya-praktikum.tech';
 
-export const serverUrl = __DOCKER_BUILD__ ? `${prodUrl}:${__SERVER_PORT__}` : 'http://localhost:3000';
+export const serverUrl = __DOCKER_BUILD__ ? `${prodUrl}:${__SERVER_PORT__}` : `http://localhost:${__SERVER_PORT__}`;
 
 export const Urls = {
     baseUrl: `${serverUrl}/api/v2`,
